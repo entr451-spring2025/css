0a7a884bf40251f775e04285c74045fc2a409681
Benjamin Block <bjblock@gmail.com>
2025-05-27 07:35:29 -0500

    api-ready

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,11 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+
+    # alternative responses to requests other than HTML
+    # respond_to do |format|
+    #   format.html # implicitly renders posts/index.html.erb
+    # end
   end
 
   def new
@@ -20,4 +25,12 @@ class PostsController < ApplicationController
     end
     redirect_to "/posts"
   end
+
+  # before_action :allow_cors
+  # def allow_cors
+  #   response.headers['Access-Control-Allow-Origin'] = '*'
+  #   response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
+  #   response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email, X-User-Token, X-User-Email'
+  #   response.headers['Access-Control-Max-Age'] = '1728000'
+  # end
 end
